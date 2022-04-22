eb9d9a723c7c145bd71abf7a4d999cf9183d2185
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-04-21 22:15:47 -0700

    change API

--- back-end/db/seeds/03_childrens_medications_seeds.sql
+++ back-end/db/seeds/03_childrens_medications_seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Acetaminophen', true, false, 150, '2022-03-23 00:00:00', '2022-03-30 00:00:00',2);
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Januvia', false, false, 1000, '2022-03-23 00:00:00', '2022-04-10 00:00:00',3);
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Dupixent', true,false, 600, NOW(), '2022-04-30 00:00:00',4);
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Farxiga', true, false, 300,'2022-03-20 00:00:00', '2022-04-10 00:00:00',5);
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Sublocade', false, false, 200, NOW(), '2022-04-15 00:00:00',1);
-INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Ibuprofen', false, false, 50, '2022-03-24 00:00:00', '2022-04-12 00:00:00',2);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Acetaminophen', true, false, 150, '2022-03-23 00:00:00', '2023-03-30 00:00:00',2);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Januvia', false, false, 1000, '2022-03-23 00:00:00', '2023-10-10 00:00:00',3);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Dupixent', true,false, 600, NOW(), '2023-04-30 00:00:00',4);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Farxiga', true, false, 300,'2022-03-20 00:00:00', '2023-10-10 00:00:00',5);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Sublocade', false, false, 200, NOW(), '2023-04-15 00:00:00',1);
+INSERT INTO childrens_medications(name, with_food, text_message, dose, start_date, end_date, child_id) VALUES ('Ibuprofen', false, false, 50, '2022-03-24 00:00:00', '2023-10-12 00:00:00',2);
 
 
