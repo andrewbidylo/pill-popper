3c641e249bf4b852f7f743c9fa27650689c2fe1c
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-04-22 10:30:49 -0700

    change path for map

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -27,7 +27,7 @@ export const Map = () => {
   });
 
   useEffect(() => {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentPosition.lat},${currentPosition.lng}&radius=2000&types=pharmacy&key=${process.env.REACT_APP_GOOGLE_KEY}`)
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentPosition.lat},${currentPosition.lng}&radius=2000&types=pharmacy&key=${process.env.REACT_APP_GOOGLE_KEY}`)
       .then((res) => {
         setData((prev) => [
           {
